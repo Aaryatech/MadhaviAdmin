584967ef9c8e170442735d01b343ee8ab971063e
ubuntu <ubuntu@ubuntu-Lenovo-ideapad-330-15IKB>
2019-11-22 18:21:35 +0530

    sac

--- src/main/java/com/ats/adminpanel/model/ewaybill/EwayConstants.java
+++ src/main/java/com/ats/adminpanel/model/ewaybill/EwayConstants.java
@@ -4,7 +4,7 @@ public class EwayConstants {
 
 	public static String genEwayGenUrl="http://testapi.taxprogsp.co.in/ewaybillapi/dec/v1.03/ewayapi?action=GENEWAYBILL&aspid=1629701119&password=pdMulani@123&gstin=05AAACG1625Q1ZK&username=05AAACG1625Q1ZK&authtoken=";
 	
-	//
+	 //
 	
 	public static String getToken="http://testapi.taxprogsp.co.in/ewaybillapi/dec/v1.03/authenticate?action=ACCESSTOKEN&aspid=1629701119&password=pdMulani@123&gstin=05AAACG1625Q1ZK&username=05AAACG1625Q1ZK&ewbpwd=abc123@@";
 	
